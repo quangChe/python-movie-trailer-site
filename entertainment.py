a1c724f9fb81d7fe8b0c78c242deb8fca5e7bf80
Quang Che <Quang@Quangs-MacBook-Pro.local>
2017-03-12 13:38:00 -0700

    Revised code and provided more useful comments

--- entertainment.py
+++ entertainment.py
@@ -1,38 +1,64 @@
+# The media module is used to create unique instances of movies and
+# the fresh_tomatoes module is used to render the movie page.
 import media
 import fresh_tomatoes
 
+# The class format can be found in: media.Movie.__doc__
+iron_man = media.Movie("Iron Man (2008)", "After being held captive in an Afg"
+                       "han cave, billionaire engineer Tony Stark creates a u"
+                       "nique weaponized suit of armor to fight evil.", "http"
+                       "s://images-na.ssl-images-amazon.com/images/M/MV5BMTcz"
+                       "NTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SY1000_C"
+                       "R0,0,674,1000_AL_.jpg", "https://www.youtube.com/watc"
+                       "h?v=8hYlB38asDY")
 
-iron_man = media.Movie("Iron Man (2008)",
-                       "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
-                       "https://images-na.ssl-images-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
-                       "https://www.youtube.com/watch?v=8hYlB38asDY")
+captain_america = media.Movie("Captain America (2011)", "Steve Rogers, a reje"
+                              "cted military soldier transforms into Captain "
+                              "America after taking a dose of a Super-Soldier"
+                              " serum. But being Captain America comes at a p"
+                              "rice as he attempts to take down a war monger "
+                              "and a terrorist organization.", "https://image"
+                              "s-na.ssl-images-amazon.com/images/M/MV5BMTYzOT"
+                              "c2NzU3N15BMl5BanBnXkFtZTcwNjY3MDE3NQ@@._V1_SY1"
+                              "000_CR0,0,640,1000_AL_.jpg", "https://www.yout"
+                              "ube.com/watch?v=jh8q4y7tbuQ")
 
-captain_america = media.Movie("Captain America (2011)",
-                              "Steve Rogers, a rejected military soldier transforms into Captain America after taking a dose of a Super-Soldier serum. But being Captain America comes at a price as he attempts to take down a war monger and a terrorist organization.",
-                              "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYzOTc2NzU3N15BMl5BanBnXkFtZTcwNjY3MDE3NQ@@._V1_SY1000_CR0,0,640,1000_AL_.jpg",
-                              "https://www.youtube.com/watch?v=jh8q4y7tbuQ")
+thor = media.Movie("Thor (2011)", "The powerful but arrogant god Thor is cast"
+                   " out of Asgard to live amongst humans in Midgard (Earth),"
+                   " where he soon becomes one of their finest defenders.",
+                   "https://images-na.ssl-images-amazon.com/images/M/MV5BOGE4"
+                   "NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQ"
+                   "XVyNTgzMDMzMTg@._V1_SY1000_CR0,0,674,1000_AL_.jpg", "http"
+                   "s://www.youtube.com/watch?v=JOddp-nlNvQ")
 
-thor = media.Movie("Thor (2011)",
-                   "The powerful but arrogant god Thor is cast out of Asgard to live amongst humans in Midgard (Earth), where he soon becomes one of their finest defenders.",
-                   "https://images-na.ssl-images-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
-                   "https://www.youtube.com/watch?v=JOddp-nlNvQ")
+the_avengers = media.Movie("The Avengers (2012)", "Earth's mightiest heroes m"
+                           "ust come together and learn to fight as a team if"
+                           " they are to stop the mischievous Loki and his al"
+                           "ien army from enslaving humanity.", "https://imag"
+                           "es-na.ssl-images-amazon.com/images/M/MV5BMTk2NTI1"
+                           "MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SY1000_C"
+                           "R0,0,675,1000_AL_.jpg", "https://www.youtube.com/"
+                           "watch?v=NPoHPNeU9fc")
 
-the_avengers = media.Movie("The Avengers (2012)",
-                          "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity.",
-                          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk2NTI1MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
-                          "https://www.youtube.com/watch?v=NPoHPNeU9fc")
+civil_war = media.Movie("CA: Civil War (2016)", "Political interference in th"
+                        "e Avengers' activities causes a rift between former "
+                        "allies Captain America and Iron Man.", "https://imag"
+                        "es-na.ssl-images-amazon.com/images/M/MV5BMjQ0MTgyNjA"
+                        "xMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SY1000_CR0,0,6"
+                        "74,1000_AL_.jpg", "https://www.youtube.com/watch?v=2"
+                        "1HP6OFn5OE")
 
-civil_war = media.Movie("CA: Civil War (2016)",
-                        "Political interference in the Avengers' activities causes a rift between former allies Captain America and Iron Man.",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
-                        "https://www.youtube.com/watch?v=21HP6OFn5OE")
+deadpool = media.Movie("Deadpool (2016)", "A fast-talking mercenary with a mo"
+                       "rbid sense of humor is subjected to a rogue experimen"
+                       "t that leaves him with accelerated healing powers and"
+                       " a quest for revenge.", "https://images-na.ssl-images"
+                       "-amazon.com/images/M/MV5BMjQyODg5Njc4N15BMl5BanBnXkFt"
+                       "ZTgwMzExMjE3NzE@._V1_SY1000_SX686_AL_.jpg", "https://"
+                       "www.youtube.com/watch?v=ONHBaC-pfsk")
 
-deadpool = media.Movie("Deadpool (2016)",
-                       "A fast-talking mercenary with a morbid sense of humor is subjected to a rogue experiment that leaves him with accelerated healing powers and a quest for revenge.",
-                       "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQyODg5Njc4N15BMl5BanBnXkFtZTgwMzExMjE3NzE@._V1_SY1000_SX686_AL_.jpg",
-                       "https://www.youtube.com/watch?v=ONHBaC-pfsk")
 
+# The open_movies_page() function from the fresh_tomatoes module
+# will load whichever movie instances we pass into it and render the
+# movies on the page.
 movies = [iron_man, captain_america, thor, the_avengers, civil_war, deadpool]
 fresh_tomatoes.open_movies_page(movies)
-
-
--- media.py
+++ media.py
@@ -1,15 +1,22 @@
-import webbrowser
-
 class Movie():
-    """This class provides a way to store and present movie related information"""
+    """
+    This class is used to create unique movie instances. Each movie
+    instance may contain four arguments.
 
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    Example:
+
+        <example1> = media.Movie('title', 'plot', 'poster url',
+                                 'trailer url')
+
+        <example2> = media.Movie('title', 'plot', 'poster url',
+                                 'trailer url')
+
+    Note:
+        This class can only take arguments in the form of a string.
+    """
 
     def __init__(self, movie_title, movie_story, movie_poster, movie_trailer):
         self.title = movie_title
         self.storyline = movie_story
         self.poster_image_url = movie_poster
         self.trailer_youtube_url = movie_trailer
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer)
